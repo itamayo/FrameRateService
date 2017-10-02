43069eb5a1195dd94f62930edabb4fcaafd3035d
itamayo <itamayo@vicomtech.org>
2017-10-02 18:44:38 +0200

    adding source

---
+++ service.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.get('/getFrameRate', function (req, res) {
+  res.send('Hello World!'+req.query.test);
+})
+
+app.listen(3500, function () {
+  console.log('Example app listening on port 3000!')
+});
+
+var  mediainfo = require('mediainfo-parser');
+function getMediaInfo(filePath) {
+  return new Promise(function(resolve, reject) {
+    mediainfo.exec(filePath, function(err, info) {
+      if(err) return reject(err);
+      return resolve(info);
+    });
+  });
+}
+
+getMediaInfo('http://localhost/vicomtech.mp4').then(function(file){
+   console.log(Math.round(file.file.track[0].frameRate));
+});
